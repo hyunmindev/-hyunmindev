36f18720759b10cb3752107f4ccfa4eb90c25e80
hyunmindev <jung@hyunmin.dev>
2024-09-17 15:39:49 +0900

    poc10

--- apps/paintest/app/(routes)/page.tsx
+++ apps/paintest/app/(routes)/page.tsx
@@ -1,24 +1,59 @@
 'use client';
 
 import { Button } from '@hyunmin-dev/ui/components/ui/button';
+import { X } from 'lucide-react';
 import { useRef, useState } from 'react';
 import {
   ReactSketchCanvas,
   type ReactSketchCanvasRef,
 } from 'react-sketch-canvas';
 
+const STROKE_COLORS = [
+  '#8B4513',
+  '#D2B48C',
+  '#228B22',
+  '#ADFF2F',
+  '#FF4500',
+  '#87CEEB',
+];
 export default function Page() {
   const [result, setResult] = useState<string>();
   const [isLoading, setIsLoading] = useState(false);
+  const [strokeColor, setStrokeColor] = useState<
+    (typeof STROKE_COLORS)[number]
+  >(STROKE_COLORS[0]);
   const canvasReference = useRef<ReactSketchCanvasRef>(null);
 
   return (
     <div className="flex size-full flex-col items-center gap-4 p-6">
+      <div className="flex flex-wrap gap-2">
+        {STROKE_COLORS.map((color) => (
+          <Button
+            className="rounded-full border"
+            key={color}
+            onClick={() => {
+              setStrokeColor(color);
+            }}
+            size="icon"
+            style={{ backgroundColor: color }}
+          />
+        ))}
+        <Button
+          className="rounded-full"
+          onClick={() => {
+            canvasReference.current?.resetCanvas();
+          }}
+          size="icon"
+          variant="outline"
+        >
+          <X className="stroke-border" />
+        </Button>
+      </div>
       <div className="h-96 w-full rounded-md border">
         <ReactSketchCanvas
           canvasColor="transparent"
           ref={canvasReference}
-          strokeColor="#a855f7"
+          strokeColor={strokeColor}
           style={{ border: 'unset' }}
           withTimestamp
         />
