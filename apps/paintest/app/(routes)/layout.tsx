369ced3bf8d28af9a1ece6ef99869ebc804752fe
hyunmindev <jung@hyunmin.dev>
2024-09-17 15:47:45 +0900

    업데이트1

--- apps/paintest/app/(routes)/layout.tsx
+++ apps/paintest/app/(routes)/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from 'next';
 
 import '@hyunmin-dev/ui/globals.css';
 import { cn } from '@hyunmin-dev/ui/libs/utils';
+import { Analytics } from '@vercel/analytics/react';
 import { GeistSans } from 'geist/font/sans';
 import { type ReactNode } from 'react';
 
 import '~/_styles/globals.css';
 
 export const metadata: Metadata = {
-  description: 'paintest',
+  description: '나무 그림을 그려보세요. 당신의 심리를 알려줄게요. 🌳',
   title: 'paintest',
 };
 
@@ -20,6 +21,7 @@ export default function Layout({ children }: Readonly<Properties>) {
   return (
     <html lang="ko">
       <body className={cn(GeistSans.className, 'm-auto max-w-screen-sm')}>
+        <Analytics />
         {children}
       </body>
     </html>
